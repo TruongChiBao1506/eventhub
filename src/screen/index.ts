d668347beb8e984cedcbe39fc85496fc92f4fce8
TruongChiBao <chibaotruong1506@gmail.com>
2024-11-05 10:10:55 +0700

    add navigator bottom tab

--- src/screen/index.ts
+++ src/screen/index.ts
@@ -4,6 +4,10 @@ import Verification from "./auth/Verification";
 import ForgotPassword from "./auth/ForgotPassword";
 import HomeScreen from "./home/HomeScreen";
 import SplashScreen from "./SplashScreen";
+import AddNewScreen from "./AddNewScreen";
+import ProfileScreen from "./profiles/ProfileScreen";
+import MapScreen from "./maps/MapScreen";
+import EventsScreen from "./events/EventsScreen";
 
 
-export {HomeScreen, LoginScreen, SplashScreen, Verification, ForgotPassword, SignUpScreen};
+export {HomeScreen, LoginScreen, SplashScreen, Verification, ForgotPassword, SignUpScreen, AddNewScreen, ProfileScreen, MapScreen, EventsScreen};
